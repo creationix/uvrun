fed9931ed15cf3f42623098b1ac2c5b8eb8201d4
Andrew Martens <andrew@nodefly.com>
2013-04-16 14:03:36 -0700

    Updated for node v0.10 - uv_run method signature changed and uv_run_once was removed

--- binding.gyp
+++ binding.gyp
@@ -1,8 +1,14 @@
 {
   "targets": [
     {
-    "target_name": "uvrun",
-    "sources": ["uvrun.cc"]
+      "target_name": "uvrun",
+      "sources": ["uvrun.cc"],
+      "variables": {
+        "node_version": '<!(node --version | sed -e "s/^v\([0-9]*\\.[0-9]*\).*$/\\1/")',
+      },
+      "target_conditions": [
+        [ "node_version == '0.8'", { "defines": ["OLD_UV_RUN_SIGNATURE"] } ]
+      ]
     }
   ]
 }
--- uvrun.cc
+++ uvrun.cc
@@ -6,13 +6,21 @@ using namespace v8;
 
 Handle<Value> Run(const Arguments& args) {
   HandleScope scope;
+#ifdef OLD_UV_RUN_SIGNATURE
   uv_run(uv_default_loop());
+#else
+    uv_run(uv_default_loop(), UV_RUN_DEFAULT);
+#endif
   return scope.Close(Null());
 }
 
 Handle<Value> RunOnce(const Arguments& args) {
   HandleScope scope;
+#ifdef OLD_UV_RUN_SIGNATURE
   int r = uv_run_once(uv_default_loop());
+#else
+  int r = uv_run(uv_default_loop(), UV_RUN_ONCE);
+#endif
   return scope.Close(Number::New(r));
 }
 
