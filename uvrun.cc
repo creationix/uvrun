6fb6e3c5725bd9bac0292577ea28f4419bc7c3b8
Albert Zeyer <albzey@gmail.com>
2014-03-25 19:41:00 +0100

    cleanup

--- uvrun.cc
+++ uvrun.cc
@@ -5,27 +5,19 @@
 using namespace v8;
 
 void Run(const FunctionCallbackInfo<Value>& args) {
-  HandleScope scope(Isolate::GetCurrent());
-#ifdef OLD_UV_RUN_SIGNATURE
-  uv_run(uv_default_loop());
-#else
+	HandleScope scope(Isolate::GetCurrent());
     uv_run(uv_default_loop(), UV_RUN_DEFAULT);
-#endif
 }
 
 void RunOnce(const FunctionCallbackInfo<Value>& args) {
-  HandleScope scope(Isolate::GetCurrent());
-#ifdef OLD_UV_RUN_SIGNATURE
-  int r = uv_run_once(uv_default_loop());
-#else
-  int r = uv_run(uv_default_loop(), UV_RUN_ONCE);
-#endif
-  args.GetReturnValue().Set(Number::New(Isolate::GetCurrent(), r));
+	HandleScope scope(Isolate::GetCurrent());
+	int r = uv_run(uv_default_loop(), UV_RUN_ONCE);
+	args.GetReturnValue().Set(Number::New(Isolate::GetCurrent(), r));
 }
 
 void init(Handle<Object> target) {
-  NODE_SET_METHOD(target, "run", Run);
-  NODE_SET_METHOD(target, "runOnce", RunOnce);
+	NODE_SET_METHOD(target, "run", Run);
+	NODE_SET_METHOD(target, "runOnce", RunOnce);
 }
 
 NODE_MODULE(uvrun, init);
