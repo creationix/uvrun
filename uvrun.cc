a652693b390eea88a51f24fa59ced7d818b2480b
Albert Zeyer <albzey@gmail.com>
2014-03-25 19:42:40 +0100

    runNoWait

--- uvrun.cc
+++ uvrun.cc
@@ -15,9 +15,16 @@ void RunOnce(const FunctionCallbackInfo<Value>& args) {
 	args.GetReturnValue().Set(Number::New(Isolate::GetCurrent(), r));
 }
 
+void RunNoWait(const FunctionCallbackInfo<Value>& args) {
+	HandleScope scope(Isolate::GetCurrent());
+	int r = uv_run(uv_default_loop(), UV_RUN_NOWAIT);
+	args.GetReturnValue().Set(Number::New(Isolate::GetCurrent(), r));
+}
+
 void init(Handle<Object> target) {
 	NODE_SET_METHOD(target, "run", Run);
 	NODE_SET_METHOD(target, "runOnce", RunOnce);
+	NODE_SET_METHOD(target, "runNoWait", RunNoWait);
 }
 
 NODE_MODULE(uvrun, init);
